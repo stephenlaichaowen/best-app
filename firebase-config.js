c632f304765b39cf49ffbce9f3a84c1de82af4f9
stephenlaichaowen <48738466+stephenlaichaowen@users.noreply.github.com>
2019-09-21 23:59:18 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBK6rTnF6u4QjKkyMtT7L8hnXABBxbxOKg",
+  authDomain: "todolist-8a5da.firebaseapp.com",
+  databaseURL: "https://todolist-8a5da.firebaseio.com",
+  projectId: "todolist-8a5da",
+  storageBucket: "todolist-8a5da.appspot.com",
+  messagingSenderId: "455537328663",
+  appId: "1:455537328663:web:86a6237e0a503069"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
